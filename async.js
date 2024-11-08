a9b33a66146136b3c4c11206d6b95cafe4ec54d4
Zekiyat Shemsu Bedewi <zekiyatatsh2009@gmail.com>
2024-11-08 10:51:44 +0300

    Add files via upload

---
+++ async.js
@@ -0,0 +1,16 @@
+// function display(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//         resolve()
+//         },2000)
+//     })
+//    }
+
+// async function hello(){
+//     console.log("hello")
+//     await display()
+//     console.log("world")
+// }
+// hello()
+
+
